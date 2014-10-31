aa7d4e621d9a78ccd6d9c8b0b5809dc93955591f
Kristof Robot <krirobo@gmail.com>
2014-10-31 13:59:25 +0100

    display_3D_visualization: improving display

--- razor_imu_9dof/nodes/display_3D_visualization.py
+++ razor_imu_9dof/nodes/display_3D_visualization.py
@@ -27,9 +27,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import roslib; roslib.load_manifest('razor_imu_9dof')
 import rospy
-
 from visual import *
 import serial
 import string
@@ -48,40 +46,38 @@ scene.range=(1.2,1.2,1.2)
 #scene.forward = (0,-1,-0.25)
 scene.forward = (1,0,-0.25)
 scene.up=(0,0,1)
-
-# Second scene (Roll, Pitch, Yaw)
-scene2 = display(title='Second Screen',x=0, y=0, width=500, height=200,center=(0,0,0), background=(0,0,0))
-scene2.range=(1,1,1)
 scene.width=500
 scene.height=500
-scene.y=200
 
+# Second scene (Roll, Pitch, Yaw)
+scene2 = display(title='9DOF Razor IMU Roll, Pitch, Yaw',x=550, y=0, width=500, height=500,center=(0,0,0), background=(0,0,0))
+scene2.range=(1,1,1)
 scene2.select()
 #Roll, Pitch, Yaw
-cil_roll = cylinder(pos=(-0.4,0,0),axis=(0.2,0,0),radius=0.01,color=color.red)
-cil_roll2 = cylinder(pos=(-0.4,0,0),axis=(-0.2,0,0),radius=0.01,color=color.red)
-cil_pitch = cylinder(pos=(0.1,0,0),axis=(0.2,0,0),radius=0.01,color=color.green)
-cil_pitch2 = cylinder(pos=(0.1,0,0),axis=(-0.2,0,0),radius=0.01,color=color.green)
-#cil_course = cylinder(pos=(0.6,0,0),axis=(0.2,0,0),radius=0.01,color=color.blue)
-#cil_course2 = cylinder(pos=(0.6,0,0),axis=(-0.2,0,0),radius=0.01,color=color.blue)
-arrow_course = arrow(pos=(0.6,0,0),color=color.cyan,axis=(-0.2,0,0), shaftwidth=0.02, fixedwidth=1)
+cil_roll = cylinder(pos=(-0.5,0.3,0),axis=(0.2,0,0),radius=0.01,color=color.red)
+cil_roll2 = cylinder(pos=(-0.5,0.3,0),axis=(-0.2,0,0),radius=0.01,color=color.red)
+cil_pitch = cylinder(pos=(0.5,0.3,0),axis=(0.2,0,0),radius=0.01,color=color.green)
+cil_pitch2 = cylinder(pos=(0.5,0.3,0),axis=(-0.2,0,0),radius=0.01,color=color.green)
+#cil_course = cylinder(pos=(0.5,0.3,0),axis=(0,0.2,0),radius=0.01,color=color.blue)
+#cil_course2 = cylinder(pos=(-0.6,0.3,0),axis=(-0.2,0,0),radius=0.01,color=color.blue)
+arrow_course = arrow(pos=(0.0,-0.4,0),color=color.cyan,axis=(0,0.2,0), shaftwidth=0.02, fixedwidth=1)
 
 #Roll,Pitch,Yaw labels
-label(pos=(-0.4,0.3,0),text="Roll",box=0,opacity=0)
-label(pos=(0.1,0.3,0),text="Pitch",box=0,opacity=0)
-label(pos=(0.55,0.3,0),text="Yaw",box=0,opacity=0)
-label(pos=(0.6,0.22,0),text="N",box=0,opacity=0,color=color.yellow)
-label(pos=(0.6,-0.22,0),text="S",box=0,opacity=0,color=color.yellow)
-label(pos=(0.38,0,0),text="W",box=0,opacity=0,color=color.yellow)
-label(pos=(0.82,0,0),text="E",box=0,opacity=0,color=color.yellow)
-label(pos=(0.75,0.15,0),height=7,text="NE",box=0,color=color.yellow)
-label(pos=(0.45,0.15,0),height=7,text="NW",box=0,color=color.yellow)
-label(pos=(0.75,-0.15,0),height=7,text="SE",box=0,color=color.yellow)
-label(pos=(0.45,-0.15,0),height=7,text="SW",box=0,color=color.yellow)
+label(pos=(-0.5,0.6,0),text="Roll",box=0,opacity=0)
+label(pos=(0.5,0.6,0),text="Pitch",box=0,opacity=0)
+label(pos=(0.0,0.02,0),text="Yaw",box=0,opacity=0)
+label(pos=(0.0,-0.16,0),text="N",box=0,opacity=0,color=color.yellow)
+label(pos=(0.0,-0.64,0),text="S",box=0,opacity=0,color=color.yellow)
+label(pos=(-0.24,-0.4,0),text="W",box=0,opacity=0,color=color.yellow)
+label(pos=(0.24,-0.4,0),text="E",box=0,opacity=0,color=color.yellow)
+label(pos=(0.18,-0.22,0),height=7,text="NE",box=0,color=color.yellow)
+label(pos=(-0.18,-0.22,0),height=7,text="NW",box=0,color=color.yellow)
+label(pos=(0.18,-0.58,0),height=7,text="SE",box=0,color=color.yellow)
+label(pos=(-0.18,-0.58,0),height=7,text="SW",box=0,color=color.yellow)
 
-L1 = label(pos=(-0.4,0.22,0),text="-",box=0,opacity=0)
-L2 = label(pos=(0.1,0.22,0),text="-",box=0,opacity=0)
-L3 = label(pos=(0.7,0.3,0),text="-",box=0,opacity=0)
+L1 = label(pos=(-0.5,0.52,0),text="-",box=0,opacity=0,height=12)
+L2 = label(pos=(0.5,0.52,0),text="-",box=0,opacity=0,height=12)
+L3 = label(pos=(0,-0.06,0),text="-",box=0,opacity=0,height=12)
 
 # Main scene objects
 scene.select()
@@ -98,7 +94,6 @@ label(pos=(0,0,-1),text="Z",box=0,opacity=0)
 platform = box(length=1, height=0.05, width=1, color=color.red)
 p_line = box(length=1,height=0.08,width=0.1,color=color.yellow)
 plat_arrow = arrow(color=color.cyan,axis=(1,0,0), shaftwidth=0.06, fixedwidth=1)
-
 
 rospy.init_node("display_3D_visualization_node")
 
@@ -126,6 +121,7 @@ def processIMU_message(rawMsg):
     cil_roll2.axis=(-0.2*cos(roll),-0.2*sin(roll),0)
     cil_pitch.axis=(0.2*cos(pitch),0.2*sin(pitch),0)
     cil_pitch2.axis=(-0.2*cos(pitch),-0.2*sin(pitch),0)
+    #cil_course.axis=(0.2*sin(yaw),0.2*cos(yaw),0)
     arrow_course.axis=(0.2*sin(yaw),0.2*cos(yaw),0)
     L1.text = str(roll)
     L2.text = str(pitch)
