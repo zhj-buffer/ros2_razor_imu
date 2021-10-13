d3aa2b3d2bf4de1c4b1156fb93a09efefc0d565e
Martin Endler <pokusew@seznam.cz>
2021-10-13 20:28:18 +0200

    Fix build deprecation warning (package.xml must be installed explicitly in setup.py)

--- setup.py
+++ setup.py
@@ -2,35 +2,50 @@ from glob import glob
 import os
 from setuptools import setup, find_packages
 
-PACKAGE_NAME = "ros2_razor_imu"
-SHARE_DIR = os.path.join("share", PACKAGE_NAME)
-
+PACKAGE_NAME = 'ros2_razor_imu'
+SHARE_DIR = os.path.join('share', PACKAGE_NAME)
 setup(
     name=PACKAGE_NAME,
     version='1.2.0',
-    packages=find_packages("src"),
+    packages=find_packages('src'),
     package_dir={'': 'src'},
     data_files=[
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + PACKAGE_NAME]),
-        (os.path.join(SHARE_DIR, "launch"), glob(os.path.join("launch", "*.launch.py"))),
-        (os.path.join(SHARE_DIR, "config"), glob(os.path.join("config", "*.yaml")))],
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + PACKAGE_NAME],
+        ),
+        (
+            SHARE_DIR,
+            ['package.xml'],
+        ),
+        (
+            os.path.join(SHARE_DIR, 'launch'),
+            glob(os.path.join('launch', '*.launch.py')),
+        ),
+        (
+            os.path.join(SHARE_DIR, 'config'),
+            glob(os.path.join('config', '*.yaml')),
+        ),
+    ],
     py_modules=[],
     zip_safe=True,
-    install_requires=['setuptools',
-                      'pyserial',
-                      'pyyaml',
-                      'transforms3d',
-                      'vpython',
-                      'wxPython'],
+    install_requires=[
+        'setuptools',
+        'pyserial',
+        'pyyaml',
+        'transforms3d',
+        'vpython',
+        'wxPython'
+    ],
     author='Kristof Robot, Tang Tiong Yew, Paul Bouchier, Peter Bartz',
     maintainer='Kristof Robot',
     keywords=['ROS2'],
     description='ros2_razor_imu is a package that provides a ROS2 driver for the Sparkfun Razor IMU 9DOF.',
     license='BSD',
     entry_points={
-        'console_scripts': ['imu_node = ros2_razor_imu.imu_node:main',
-                            'display_3D_visualization_node = ros2_razor_imu.display_3D_visualization:main'
-                            ],
-    }
+        'console_scripts': [
+            'imu_node = ros2_razor_imu.imu_node:main',
+            'display_3D_visualization_node = ros2_razor_imu.display_3D_visualization:main',
+        ],
+    },
 )
